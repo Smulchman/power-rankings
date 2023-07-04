4d5d9a7a59e06dafd5f71f60d0c721894688be24
smulch <mulcahy.samuel@gmail.com>
2023-07-04 14:44:02 -0400

    added getmatchups and getplayers api calls

--- controllers/api/sleeper.js
+++ controllers/api/sleeper.js
@@ -14,4 +14,28 @@ async function getRosters() {
   }
 }
 
-module.exports = { getRosters };
+async function getMatchups(week) {
+  try {
+    const response = await axios.get(
+      `https://api.sleeper.app/v1/league/859465702640300032/matchups/${week}`
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getPlayers() {
+  try {
+    const response = await axios.get(
+      `https://api.sleeper.app/v1/players/nfl`
+    );
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { getRosters, getMatchups, getPlayers };
