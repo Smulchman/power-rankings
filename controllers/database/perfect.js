214baa2b28986f9cbcef04ea608686bca258c299
smulch <mulcahy.samuel@gmail.com>
2023-07-25 18:37:59 -0400

    database controller (is this right?)

---
+++ controllers/database/perfect.js
@@ -0,0 +1,7 @@
+// use get request to database to get data
+// need info from database and from API.
+
+// take team data from API, cross reference with player data from database, and return a list of players on each team
+// hold onto the ID so that I can store the 'perfect' score in the db properly
+// run player data through algorithm to determine the perfect lineup
+// return perfect lineup to database
